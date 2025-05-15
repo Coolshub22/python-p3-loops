7e84e8ef8470976ac4268eb99279c14e4cc979a0
Arnold <arnoldkulavi@gmail.com>
2025-05-15 13:44:32 +0300

    feat:loop

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count >= 1:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
+
+print(happy_new_year())
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [int_list ** 2 for int_list in int_list]
+
+print(square_integers([1, 2, 3, 4]))
+print(square_integers([-2, 0, 5]))
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for number in range(1,101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+
+print(fizzbuzz())
+
+
+
